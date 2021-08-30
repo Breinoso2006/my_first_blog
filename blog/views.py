669c6533e5be4157bd2b50f512f2c44294e923d9
bruno <bruno.teixeira@dafiti.com.br>
2021-08-30 13:39:47 -0300

    Utilizando pylint e black

--- blog/views.py
+++ blog/views.py
@@ -1,44 +1,58 @@
+"""Arquivo de views do projeto.
+Serve para acessar os templates de acordo com o
+requisitado."""
+
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from blog.models import Post
 from blog.forms import PostForm
+# pylint: disable=invalid-name
 
 # Create your views here.
 
 def post_list(request):
+    """Fornece a lista de Posts registrados de acordo com a data especificada"""
+
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts' : posts})
 
 def post_detail(request, pk):
+    """Fornece detalhes do Post em questão"""
+
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_new(request):
-     if request.method == "POST":
-         form = PostForm(request.POST)
-         if form.is_valid():
-             post = form.save(commit=False)
-             post.author = request.user
-             post.published_date = timezone.now()
-             post.save()
-             return redirect('post_detail', pk=post.pk)
-     else:
-         form = PostForm()
-     return render(request, 'blog/post_edit.html', {'form': form})
+    """Usado para a criação de um novo Post"""
+
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
 
 def post_edit(request, pk):
-     post = get_object_or_404(Post, pk=pk)
-     if request.method == "POST":
-         form = PostForm(request.POST, instance=post)
-         if form.is_valid():
-             post = form.save(commit=False)
-             post.author = request.user
-             post.published_date = timezone.now()
-             post.save()
-             return redirect('post_detail', pk=post.pk)
-     else:
-         form = PostForm(instance=post)
-     return render(request, 'blog/post_edit.html', {'form': form})
+    """Usado para a edição de um Post já existente"""
+
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
 
 def about(request):
+    """Acesso a página About"""
     return render(request, 'blog/about.html', {})
